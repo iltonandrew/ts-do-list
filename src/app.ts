9d5a84f254f4ac7bfa640d0065a3aceb7cfd1387
iltonandrew <iltonandrew@gmail.com>
2021-10-17 23:19:16 -0300

    ✨Feat: Add delete todo

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ enum Commands {
   Exit = 0,
   Add,
   Edit,
+  Delete
 }
 
 function DisplayList(list: Array<string>) {
@@ -21,7 +22,7 @@ const question = util.promisify(rl.question).bind(rl);
 const todoList: Array<string> = [];
 
 async function Main() {
-  const option = await question("Type 1 to add, 0 to exit: ");
+  const option = await question("Choose option:\n1- Add\n2- Edit\n3- Delete\n0- Exit\n");
   if (option == Commands.Exit) return rl.close();
 
   if (option == Commands.Add) {
@@ -35,13 +36,24 @@ async function Main() {
     console.log("Current list:");
     DisplayList(todoList);
     const itemToEdit: number = await question(
-      "Type the task numbem you want to edit: "
+      "Type the task number you want to edit: "
     );
     const newItemValue: string = await question(
       `Type the new value for the item ${itemToEdit}: `
     );
     todoList[itemToEdit] = newItemValue
 
+    console.log("Current list:");
+    DisplayList(todoList);
+  }
+
+  if (option == Commands.Delete){
+    console.log("Current list:");
+    DisplayList(todoList);
+    const itemToRemove: number = await question(
+      "Type the task number you want to remove: "
+    );
+    todoList.splice(itemToRemove, 1)
     console.log("Current list:");
     DisplayList(todoList);
   }
